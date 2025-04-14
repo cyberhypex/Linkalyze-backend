32bf7ea94df93a174da13a107faa611718e47ebd
Anshuman <chaoanxuman999@gmail.com>
2025-04-14 23:15:29 +0530

    Linkalyze-a bitly clone with analytics backend setup

---
+++ src/main/java/com/Link/Linkalyz/LinkalyzApplication.java
@@ -0,0 +1,13 @@
+package com.Link.Linkalyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkalyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkalyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Link/Linkalyz/LinkalyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Link.Linkalyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkalyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
