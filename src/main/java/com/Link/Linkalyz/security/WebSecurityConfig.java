060efa9f16088103a11eb32da25817f5c08fe289
Anshuman <chaoanxuman999@gmail.com>
2025-05-01 10:17:44 +0530

    Fixed an error where charts would throw a cors error

--- src/main/java/com/Link/Linkalyz/security/WebSecurityConfig.java
+++ src/main/java/com/Link/Linkalyz/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.Link.Linkalyz.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -45,7 +46,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable).
                 authorizeHttpRequests(auth->
-                        auth.requestMatchers("/api/auth/**").permitAll()
+                        auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/urls/**").authenticated()
                 .requestMatchers("/{shortUrl}").permitAll()
                                 .anyRequest().authenticated()
